e250d2bcd6891387beb46635fd21438ed5dc879b
Rishabh <rishabh31121999@gmail.com>
2018-07-15 11:01:40 +0530

    [30DaysOfCode]Add day 11 Solution

---
+++ 30DaysOfCode/day11.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+    
+    public static int getMaxSumHourGlasses(int arr[][]){
+        int max = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2];
+        for(int i=0;i<=3;i++){
+            for(int j=0;j<=3;j++){
+                int curr = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                if(curr>max){
+                    max = curr;
+                }
+            }
+        }
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        // Input code
+        int[][] arr = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        
+        // Solution
+        System.out.println(getMaxSumHourGlasses(arr));
+
+        scanner.close();
+    }
+}
